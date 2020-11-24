66d9445799013db4b15042dfa29fecb11769b4d3
Danny Delott <dannydelott@gmail.com>
2020-11-24 01:30:20 -0800

    formatting

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { inspect } = require("util");
 const core = require("@actions/core");
 const github = require("@actions/github");
-const fs = require('fs');
+const fs = require("fs");
 
 async function run() {
   try {
@@ -12,7 +12,7 @@ async function run() {
     };
 
     const {
-      payload: { pull_request: pullRequest, repository }
+      payload: { pull_request: pullRequest, repository },
     } = github.context;
 
     if (!pullRequest) {
@@ -26,19 +26,31 @@ async function run() {
 
     const octokit = new github.getOctokit(inputs.token);
 
-    const data = fs.readFileSync(`${process.env.GITHUB_WORKSPACE}/${inputs.path}`, 'utf8');
+    const data = fs.readFileSync(
+      `${process.env.GITHUB_WORKSPACE}/${inputs.path}`,
+      "utf8"
+    );
     const json = JSON.parse(data);
 
-    const coverage = `####${inputs.title}
-| Statements | Branches | Functions | Lines |     
-|---|---|---|---|     
-|${json.total.statements.pct}% ( ${json.total.statements.covered}/${json.total.statements.total} ) | ${json.total.branches.pct}%   ( ${json.total.branches.covered}  /${json.total.branches.total} ) | ${json.total.functions.pct}%  ( ${json.total.functions.covered} / ${json.total.functions.total} ) | ${json.total.lines.pct}%      ( ${json.total.lines.covered}     /${json.total.lines.total} )|`
+    const totalStatementsPct = json.total.statements.pct;
+    const totalStatementsCovered = json.total.statements.covered;
+    const totalStatementsTotal = json.total.statements.total;
+
+    const totalBranchesPct = json.total.branches.pct;
+    const totalBranchesCovered = json.total.branches.covered;
+    const totalBranchesTotal = json.total.branches.total;
+    const coverage = [
+      `#### ${inputs.title}`,
+      `| Statements | Branches | Functions | Lines |`,
+      `|---|---|---|---|`,
+      `| ${totalStatementsPct}% (${totalStatementsCovered} / ${totalStatementsTotal}) | ${totalBranchesPct}% (${totalBranchesCovered} / ${totalBranchesTotal}) | ${json.total.functions.pct}%  (${json.total.functions.covered} / ${json.total.functions.total}) | ${json.total.lines.pct}% (${json.total.lines.covered} / ${json.total.lines.total}) |`,
+    ].join("\n");
 
     await octokit.issues.createComment({
       owner,
       repo,
       issue_number: issueNumber,
-      body: eval('`' + coverage + '`')
+      body: eval("`" + coverage + "`"),
     });
   } catch (error) {
     core.debug(inspect(error));
